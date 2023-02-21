1cb407dba53dfe8ac1047fb9529fe15613d84e02
bondanjody <bondanjodys@gmail.com>
2023-02-21 12:16:18 +0700

    Menambahkan reset logic (ketika form di submit maka input field akan tereset). Menambahkan pengkondisian untuk menyaring apabila input field kosong, dan apabila umur <= 0.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
+    const [usernameValue, setUsernameValue] = useState('');
+    const [ageValue, setAgeValue] = useState('');
+
+    const usernameHandler = event => {
+        setUsernameValue(event.target.value);
+    }
+
+    const ageHandler = event => {
+        setAgeValue(event.target.value);
+    }
+
     const submitHandler = (event) => {
         event.preventDefault();
-        
+        if (usernameValue.trim().length === 0 || ageValue.trim().length === 0) {
+            return;
+        }
+        if (+ageValue <= 0) {
+            return;
+        }
+        console.log(`Username : ${usernameValue}. Age : ${ageValue}`);
+        setUsernameValue('');
+        setAgeValue('');
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={submitHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" value={usernameValue} onChange={usernameHandler} type="text" />
                 <label htmlFor="age">Age (year)</label>
-                <input id="age" type="number" />
-                <Button>Add User</Button>
+                <input id="age" value={ageValue} onChange={ageHandler} type="number" />
+                <Button type="submit">Add User</Button>
             </form>
         </Card>  
     )

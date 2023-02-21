444733665b6ef75d5477a596e193c7b09b99c689
bondanjody <bondanjodys@gmail.com>
2023-02-21 11:16:49 +0700

    Inisial commit. Membuat folder components. Membuat folder UI dan User. Menambahkan file AddUser.js

---
+++ src/components/User/AddUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddUser = () => {
+    return (
+        <form>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+        </form>
+    )
+}
+
+export default AddUser;

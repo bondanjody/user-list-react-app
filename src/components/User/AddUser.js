f83c02799d83ddae2f2721ae0f23be5645cd9319
bondanjody <bondanjodys@gmail.com>
2023-02-21 11:52:51 +0700

    Menambahkan komponen Button.js beserta stylingnya. Memanggil component Button di Component AddUser.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
+
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
@@ -15,7 +17,7 @@ const AddUser = () => {
                 <input id="username" type="text" />
                 <label htmlFor="age">Age (year)</label>
                 <input id="age" type="number" />
-                <button>Add User</button>
+                <Button>Add User</Button>
             </form>
         </Card>  
     )

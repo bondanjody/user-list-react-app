d7a2bed5bb8f038b74a67b848d3cec4708c39169
bondanjody <bondanjodys@gmail.com>
2023-02-21 12:50:55 +0700

    Menambahkan component UserList yang khusus untuk menampilkan list user yang sudah dimasukkan.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AddUser from './components/User/AddUser';
+import UsersList from './components/User/UsersList';
 
 
 function App() {
+
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }

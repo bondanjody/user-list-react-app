e762be670540060b225e777dd053a903bcb802d0
bondanjody <bondanjodys@gmail.com>
2023-02-21 11:39:24 +0700

    Menambahkan Card.js dan Card.module.css. Membuat dua file CSS Modules dari dua component yang berbeda dapat digunakan sebagaimana mestinya dengan menggunakan template literals.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AddUser from './components/User/AddUser';
 
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
 
 const AddUser = () => {
+    const submitHandler = (event) => {
+        event.preventDefault();
+        
+    }
+
     return (
-        <form>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={submitHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (year)</label>
+                <input id="age" type="number" />
+                <button>Add User</button>
+            </form>
+        </Card>  
     )
 }
 

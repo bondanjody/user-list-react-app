9dc419a841533eb61a327eacb921b0a489cc66ec
bondanjody <bondanjodys@gmail.com>
2023-02-22 20:37:59 +0700

    Mengimplementasikan React.fragment dan ReactDOM.createPortal. Portal memungkinkan kita untuk memilih target elemen HTML secara spesifik untuk component kita. Misalnya kita ingin component ErrorModal tidak berada di dalam sebuah elemen HTML (nested).

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from 'react-dom';
 
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+}
 
-const ErrorModal = props => {
-    return (
-        <div className={classes.backdrop}>
-            <Card className={classes.modal}>
+const ModalOverlay = props => {
+    return <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
                 </header>
@@ -18,9 +20,15 @@ const ErrorModal = props => {
                 <div className={classes.actions}>
                     <Button onClick={props.onOK}>OK</Button>
                 </div>
-                
             </Card>
-        </div>
+}
+
+const ErrorModal = props => {
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onOK} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.title} onOK={props.onOK} />, document.getElementById('modaloverlay-root'))}
+        </React.Fragment>
     );
 }
 
